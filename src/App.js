e482b32bd65d6eff8e35766d4b04bfabd698d680
komals1402 <ks387406@gmail.com>
2023-06-29 12:46:26 +0530

    created public and privateroute

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
-import 'rsuite/dist/styles/rsuite-default.css';
+import React from 'react';
+
 import './styles/main.scss';
+import { Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import PublicRoute from './components/PublicRoute';
+import SignIn from './pages/SignIn';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin>">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
